4a2dca3c9077c612812d1007e7eb60e3b9cd3c8e
2137772-Lamarche-Felix <2137772@carrefour.cegepvicto.ca>
2023-12-15 16:26:53 -0500

    Ajout PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+plugins: [
+    react(),
+    VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+        enabled: true,
+    },
+    includeAssets: ['favicon.ico', 'logo192.png', 'vite.svg'],
+    manifest: {
+        name: 'Musik Web 3',
+        short_name: 'Musik',
+        description: 'Gere ta musique comme tu le souhaite',
+        theme_color: '#ffffff',
+        icons: [
+        {
+            src: 'https://cdn-icons-png.flaticon.com/512/68/68197.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+        },
+        {
+            src: 'https://cdn-icons-png.flaticon.com/512/68/68197.png',
+            sizes: '512x512',
+            type: 'image/png',
+        },
+        ],
+    },
+    }),
+],
+});
